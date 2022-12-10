a96e8eee46d8fec5f9afe449ab3e6922db7f7cbd
LDG - DGit <hi-ldg@dgit.co>
2022-12-11 01:51:37 +0900

    init

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#202020",
+  textColor: "#ffffff",
+  accentColor: "#00ee9b",
+  cardColor: "#000000",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f0f0f0",
+  textColor: "#000000",
+  accentColor: "#00ee9b",
+  cardColor: "#ffffff",
+};
