a6ced386cb472fc34e48830b0977b4fad3f2f5d2
LDG - DGit <hi-ldg@dgit.co>
2022-12-11 02:04:39 +0900

    create todo component

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,24 @@
+import React, { useState } from "react";
+
 function ToDoList() {
-  return null;
+  const [todo, setTodo] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setTodo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={todo} onChange={onChange} placeholder='Write a to do' />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
