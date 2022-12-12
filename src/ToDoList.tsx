502586ca274c512dec488511718879ec99a00519
LDG - DGit <hi-ldg@dgit.co>
2022-12-12 21:53:37 +0900

    Practice React Hook Form Custom Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -29,12 +29,14 @@ interface IFormData {
       message: string;
     };
   };
+  extraError?: string;
 }
 
 function TodoList() {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm<IFormData>({
     defaultValues: {
@@ -42,17 +44,27 @@ function TodoList() {
     },
   });
   const onValid = (data: any) => {
-    console.log(data);
+    if (data.todo === "Here!") {
+      setError("todo", { message: "It is Default Value." }, { shouldFocus: true });
+    }
+    //setError("extraError", { message: "Server offline." });
   };
   return (
     <div>
       <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
         <span>{errors?.todo?.message}</span>
         <input
-          {...register("todo", { required: "todo is required" })}
+          {...register("todo", {
+            required: "Todo is required",
+            validate: {
+              badword: (value) => (value.includes("Bad") ? "no bad words allowed" : true),
+              badword2: (value) => (value.includes("bad") ? "no bad words allowed" : true),
+            },
+          })}
           placeholder='Write a to do'
         />
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
